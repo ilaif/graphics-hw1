27ebdd1e51d91fa36de237b43b2dece4d3f5b5ce
Ilai Fallach <ifallach@gmail.com>
2017-03-31 15:29:45 +0300

    [ADD] Testing image loading in Main.java

--- src/graphics/hw1/Main.java
+++ src/graphics/hw1/Main.java
@@ -1,8 +1,28 @@
 package graphics.hw1;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        BufferedImage img = null;
+        File f = new File("./images/sunset-horizon.jpg");
+        if (!f.exists()) {
+            System.out.println("File does not exist");
+            return;
+        }
+        try {
+            img = ImageIO.read(f);
+        } catch (IOException e) {
+            System.out.println("Error loading image, error: " + e.toString());
+        }
+
+        System.out.println(img.toString());
+
     }
 }
